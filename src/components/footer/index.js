ae6c608f32e110f602025cbe1a4b3ee5f251905f
Hadi <nasshitell@gmail.com>
2021-07-29 19:31:53 +0700

    navbar footer

---
+++ src/components/footer/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./index.css";
+
+const Footer = () => {
+  return (
+    <>
+      <div className="footer-container">
+        <div className="footer-main-row">
+          <div className="footer-main-column footer-main-left">
+            asdasdasdasdasd
+          </div>
+          <div className="footer-main-column footer-main-right">
+            ASIDUHAKUWDHKUWDHUAKDW
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
