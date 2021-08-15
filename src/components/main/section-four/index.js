47c205b92d2a501e2b24977448392916b6cafbe7
Hadi <nasshitell@gmail.com>
2021-08-15 20:57:41 +0700

    last push 15 agt

--- src/components/main/section-four/index.js
+++ src/components/main/section-four/index.js
@@ -46,288 +46,148 @@ const SectionFour = () => {
 
   return (
     <>
-      {button ? (
-        <div className="section-four-container">
-          <div className="section-four-row">
-            <div className="section-four-column four-left">
-              <div className="section-four-image-container">
-                <img src={mockup} alt="mockup" className="section-four-image" />
-              </div>
+      <div className="section-four-container">
+        <div className="section-four-row">
+          <div className="section-four-column four-left">
+            <div className="section-four-image-container">
+              <img src={mockup} alt="mockup" className="section-four-image" />
             </div>
-            <div className="section-four-column four-right">
-              <div className="section-four-title">
-                Journey with my
-                <span className="section-four-title-red">hero!</span>
-              </div>
-              <div className="section-four-steps-row">
-                <div className="section-four-steps-column steps-left">
-                  <div className="section-four-steps-container">
+          </div>
+          <div className="section-four-column four-right">
+            <div className="section-four-title">
+              Journey with my
+              <span className="section-four-title-red">hero!</span>
+            </div>
+            <div className="section-four-steps-row">
+              <div className="section-four-steps-column steps-left">
+                <div className="section-four-steps-container">
+                  <div
+                    className={
+                      stepOne
+                        ? "section-four-step-opened"
+                        : "section-four-step-closed"
+                    }
+                    onClick={() => openStepOneOnly()}
+                  >
                     <div
                       className={
                         stepOne
-                          ? "section-four-step-opened"
-                          : "section-four-step-closed"
+                          ? "section-four-step-title-opened"
+                          : "section-four-step-title-closed"
                       }
-                      onClick={() => openStepOneOnly()}
                     >
-                      <div
-                        className={
-                          stepOne
-                            ? "section-four-step-title-opened"
-                            : "section-four-step-title-closed"
-                        }
-                      >
-                        Step One
-                      </div>
-                      {stepOne ? (
-                        <>
-                          <div className="section-four-step-content-row">
-                            <div className="section-four-step-content-column step-content-left">
-                              <div className="section-four-step-description">
-                                Nullam mollis sed magna et hendrerit. sit amet
-                                ante iaculis eget urna egetfeugiat.
-                              </div>
-                            </div>
-                            <div className="section-four-step-content-column step-content-right">
-                              <Link
-                                to="/"
-                                className="section-four-step-content-button"
-                              >
-                                Learn More
-                              </Link>
+                      Step One
+                    </div>
+                    {stepOne ? (
+                      <>
+                        <div className="section-four-step-content-row">
+                          <div className="section-four-step-content-column step-content-left">
+                            <div className="section-four-step-description">
+                              Nullam mollis sed magna et hendrerit. sit amet
+                              ante iaculis eget urna egetfeugiat.
                             </div>
                           </div>
-                        </>
-                      ) : null}
-                    </div>
+                          <div className="section-four-step-content-column step-content-right">
+                            <Link
+                              to="/"
+                              className="section-four-step-content-button"
+                            >
+                              Learn More
+                            </Link>
+                          </div>
+                        </div>
+                      </>
+                    ) : null}
+                  </div>
+                  <div
+                    className={
+                      stepTwo
+                        ? "section-four-step-opened"
+                        : "section-four-step-closed"
+                    }
+                    onClick={() => openStepTwoOnly()}
+                  >
                     <div
                       className={
                         stepTwo
-                          ? "section-four-step-opened"
-                          : "section-four-step-closed"
+                          ? "section-four-step-title-opened"
+                          : "section-four-step-title-closed"
                       }
-                      onClick={() => openStepTwoOnly()}
                     >
-                      <div
-                        className={
-                          stepTwo
-                            ? "section-four-step-title-opened"
-                            : "section-four-step-title-closed"
-                        }
-                      >
-                        Step Two
-                      </div>
-                      {stepTwo ? (
-                        <>
-                          <div className="section-four-step-content-row">
-                            <div className="section-four-step-content-column step-content-left">
-                              <div className="section-four-step-description">
-                                Nullam mollis sed magna et hendrerit. sit amet
-                                ante iaculis eget urna egetfeugiat.
-                              </div>
-                            </div>
-                            <div className="section-four-step-content-column step-content-right">
-                              <Link
-                                to="/"
-                                className="section-four-step-content-button"
-                              >
-                                Learn more
-                              </Link>
+                      Step Two
+                    </div>
+                    {stepTwo ? (
+                      <>
+                        <div className="section-four-step-content-row">
+                          <div className="section-four-step-content-column step-content-left">
+                            <div className="section-four-step-description">
+                              Nullam mollis sed magna et hendrerit. sit amet
+                              ante iaculis eget urna egetfeugiat.
                             </div>
                           </div>
-                        </>
-                      ) : null}
-                    </div>
+                          <div className="section-four-step-content-column step-content-right">
+                            <Link
+                              to="/"
+                              className="section-four-step-content-button"
+                            >
+                              Learn more
+                            </Link>
+                          </div>
+                        </div>
+                      </>
+                    ) : null}
+                  </div>
+                  <div
+                    className={
+                      stepThree
+                        ? "section-four-step-opened"
+                        : "section-four-step-closed"
+                    }
+                    onClick={() => openStepThreeOnly()}
+                  >
                     <div
                       className={
                         stepThree
-                          ? "section-four-step-opened"
-                          : "section-four-step-closed"
+                          ? "section-four-step-title-opened"
+                          : "section-four-step-title-closed"
                       }
-                      onClick={() => openStepThreeOnly()}
                     >
-                      <div
-                        className={
-                          stepThree
-                            ? "section-four-step-title-opened"
-                            : "section-four-step-title-closed"
-                        }
-                      >
-                        Last Step
-                      </div>
-                      {stepThree ? (
-                        <>
-                          <div className="section-four-step-content-row">
-                            <div className="section-four-step-content-column step-content-left">
-                              <div className="section-four-step-description">
-                                Nullam mollis sed magna et hendrerit. sit amet
-                                ante iaculis eget urna egetfeugiat.
-                              </div>
-                            </div>
-                            <div className="section-four-step-content-column step-content-right">
-                              <Link
-                                to="/"
-                                className="section-four-step-content-button"
-                              >
-                                Learn more
-                              </Link>
+                      Last Step
+                    </div>
+                    {stepThree ? (
+                      <>
+                        <div className="section-four-step-content-row">
+                          <div className="section-four-step-content-column step-content-left">
+                            <div className="section-four-step-description">
+                              Nullam mollis sed magna et hendrerit. sit amet
+                              ante iaculis eget urna egetfeugiat.
                             </div>
                           </div>
-                        </>
-                      ) : null}
-                    </div>
+                          <div className="section-four-step-content-column step-content-right">
+                            <Link
+                              to="/"
+                              className="section-four-step-content-button"
+                            >
+                              Learn more
+                            </Link>
+                          </div>
+                        </div>
+                      </>
+                    ) : null}
                   </div>
                 </div>
-                <div className="section-four-steps-column steps-right">
-                  <span className={stepOne ? "dot-large" : "dot"}></span>
-                  <br />
-                  <span className={stepTwo ? "dot-large" : "dot"}></span>
-                  <br />
-                  <span className={stepThree ? "dot-large" : "dot"}></span>
-                </div>
+              </div>
+              <div className="section-four-steps-column steps-right">
+                <span className={stepOne ? "dot-large" : "dot"}></span>
+                <br />
+                <span className={stepTwo ? "dot-large" : "dot"}></span>
+                <br />
+                <span className={stepThree ? "dot-large" : "dot"}></span>
               </div>
             </div>
           </div>
         </div>
-      ) : (
-        <div className="section-four-container">
-          <div className="section-four-row">
-            <div className="section-four-column">
-              <div className="section-four-title">
-                Journey with my
-                <span className="section-four-title-red">hero!</span>
-              </div>
-              <div className="section-four-steps-row">
-                <div className="section-four-steps-column steps-left">
-                  <div className="section-four-steps-container">
-                    <div
-                      className={
-                        stepOne
-                          ? "section-four-step-opened"
-                          : "section-four-step-closed"
-                      }
-                      onClick={() => openStepOneOnly()}
-                    >
-                      <div
-                        className={
-                          stepOne
-                            ? "section-four-step-title-opened"
-                            : "section-four-step-title-closed"
-                        }
-                      >
-                        Step One
-                      </div>
-                      {stepOne ? (
-                        <>
-                          <div className="section-four-step-content-row">
-                            <div className="section-four-step-content-column step-content-left">
-                              <div className="section-four-step-description">
-                                Nullam mollis sed magna et hendrerit. sit amet
-                                ante iaculis eget urna egetfeugiat.
-                              </div>
-                            </div>
-                            <div className="section-four-step-content-column step-content-right">
-                              <Link
-                                to="/"
-                                className="section-four-step-content-button"
-                              >
-                                Learn More
-                              </Link>
-                            </div>
-                          </div>
-                        </>
-                      ) : null}
-                    </div>
-                    <div
-                      className={
-                        stepTwo
-                          ? "section-four-step-opened"
-                          : "section-four-step-closed"
-                      }
-                      onClick={() => openStepTwoOnly()}
-                    >
-                      <div
-                        className={
-                          stepTwo
-                            ? "section-four-step-title-opened"
-                            : "section-four-step-title-closed"
-                        }
-                      >
-                        Step Two
-                      </div>
-                      {stepTwo ? (
-                        <>
-                          <div className="section-four-step-content-row">
-                            <div className="section-four-step-content-column step-content-left">
-                              <div className="section-four-step-description">
-                                Nullam mollis sed magna et hendrerit. sit amet
-                                ante iaculis eget urna egetfeugiat.
-                              </div>
-                            </div>
-                            <div className="section-four-step-content-column step-content-right">
-                              <Link
-                                to="/"
-                                className="section-four-step-content-button"
-                              >
-                                Learn more
-                              </Link>
-                            </div>
-                          </div>
-                        </>
-                      ) : null}
-                    </div>
-                    <div
-                      className={
-                        stepThree
-                          ? "section-four-step-opened"
-                          : "section-four-step-closed"
-                      }
-                      onClick={() => openStepThreeOnly()}
-                    >
-                      <div
-                        className={
-                          stepThree
-                            ? "section-four-step-title-opened"
-                            : "section-four-step-title-closed"
-                        }
-                      >
-                        Last Step
-                      </div>
-                      {stepThree ? (
-                        <>
-                          <div className="section-four-step-content-row">
-                            <div className="section-four-step-content-column step-content-left">
-                              <div className="section-four-step-description">
-                                Nullam mollis sed magna et hendrerit. sit amet
-                                ante iaculis eget urna egetfeugiat.
-                              </div>
-                            </div>
-                            <div className="section-four-step-content-column step-content-right">
-                              <Link
-                                to="/"
-                                className="section-four-step-content-button"
-                              >
-                                Learn more
-                              </Link>
-                            </div>
-                          </div>
-                        </>
-                      ) : null}
-                    </div>
-                  </div>
-                </div>
-                <div className="section-four-steps-column steps-right">
-                  <span className={stepOne ? "dot-large" : "dot"}></span>
-                  <br />
-                  <span className={stepTwo ? "dot-large" : "dot"}></span>
-                  <br />
-                  <span className={stepThree ? "dot-large" : "dot"}></span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
+      </div>
     </>
   );
 };
